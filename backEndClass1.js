c9527585449e61a9f5b549afacdbabeaa4a09f42
arielzybala <repobackend@gmail.com>
2022-03-18 10:28:58 -0300

    Update BackEndClass1.js
    Se fueron practicando distintas tareas en la segunda clase, que se pueden incorporar en class1

--- backEndClass1.js
+++ backEndClass1.js
@@ -1,2 +1,16 @@
-let a = "hola";
-console.log(a);
+//let a = [1, 2, 3];
+//console.log(a.length);
+let lista = [];
+function mostrarLista (lista) {
+    if (lista.length === 0){
+        console.log("lista vacía");
+    } else {
+        console.log(lista);
+    }
+}
+mostrarLista(lista);
+
+let lista2 = ["manzana", "naranja", "frutilla"];
+const mostrarLista2 = (lista2) =>{ lista2.length === 0 ? console.log("lista vacía") :  console.log(lista2)}
+
+mostrarLista2(lista2);

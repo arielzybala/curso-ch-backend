ebde2c63be0b80c886b928d76cb6497c1ff13e4c
arielzybala <repobackend>
2022-08-20 16:46:49 -0300

    DesafíoClase34
    Intentando dar inicio

--- src/app.js
+++ src/app.js
@@ -4,14 +4,14 @@ const http = require("http");
 const yargs = require('yargs/yargs')(process.argv.slice(2))
 const args = yargs
 .alias({p: 'PORT', m: 'MODO' }).argv 
-const PORT = args.PORT;
+const PORT = args.PORT || process.env.PORT || 8080;
 const app = express()
 const server = http.createServer(app);
 const routerMain = require("./routes/mainRoutes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(__dirname + '/public'));
 
 app.set('json spaces', 1)
 app.set("view engine", "ejs");

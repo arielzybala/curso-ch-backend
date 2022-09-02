dbd929b081f2474434787833332c1fa6e4335e16
arielzybala <repobackend>
2022-09-02 02:09:15 -0300

    PEF3
    TEST Subidos. Modo CLUSTER - FORK armado

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 const NODE_ENV = process.env.NODE_ENV || "development";
-require("dotenv").config({ path: `.env.${NODE_ENV}` });//Esto es por si hay que usar un .env.production, en algún momento
+require("dotenv").config({ path: `.env.${NODE_ENV}` }); //Esto es por si hay que usar un .env.production, en algún momento
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
 const cp = require("cookie-parser");
-const cors = require("cors")
+const cors = require("cors");
 const mongoStore = require("connect-mongo");
 const http = require("http");
-const cluster = require('cluster')
-const {cpus} = require('os')
+const cluster = require("cluster");
+const { cpus } = require("os");
+const compression = require("compression");
 const yargs = require("yargs/yargs")(process.argv.slice(2));
 const args = yargs.alias({ p: "PORT" }).argv;
 const PORT = args.PORT || process.env.PORT || 8080; //Quedo modificado así por Heroku
@@ -23,10 +24,11 @@ const routerProducts = require("./routes/productsRoutes");
 const passport = require("./routes/middleware/passport");
 const { logger } = require("./utils/logger");
 
+app.use(compression());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
-app.use(cors())
+app.use(cors());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname + "/views"));
@@ -39,7 +41,7 @@ app.use(
       mongoOptions: mongoAtlas.advancedOptions,
       ttl: 60,
       retries: 0,
-      touchAfter: 24 * 3600,//con esto la sesión dura 24hs
+      touchAfter: 24 * 3600, //con esto la sesión dura 24hs
     }),
     secret: mongoAtlas.secret,
     resave: true,
@@ -56,30 +58,31 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use("/", routerMain, routerUser);
 app.use("/api/cart", routerCart);
-app.use("/api/products", routerProducts)
+app.use("/api/products", routerProducts);
 
-
-if(process.env.SERVERMODE == "FORK"){
+if (process.env.SERVERMODE == "FORK") {
   server.listen(PORT, (err) => {
     !err
-      ? logger.info(`Server iniciado en modo:[${process.env.SERVERMODE}] con id de Proceso [${process.pid}] en puesto [${PORT}]`)
+      ? logger.info(
+          `Server iniciado en modo:[${process.env.SERVERMODE}] con id de Proceso [${process.pid}] en puerto [${PORT}]`
+        )
       : logger.error(`Server no funciona por error:[${err}`);
   });
 } else {
-  if(cluster.isMaster){
+  if (cluster.isMaster) {
     for (let i = 0; i < numCPUS; i++) {
-        cluster.fork()
+      cluster.fork();
     }
-    cluster.on('exit', (worker,code,signal)=>{
-        logger.info(`worker con id de proceso ${worker.process.pid} eliminado`)
-        //para mantener una cantidad de cluster activos acá se puede poner un
-        cluster.fork()
-        // sino al eliminar el último cluster las request al sitio por parte del cliente no tendran un cpu que lo procese
-    })
-} else if(process.env.SERVERMODE == "CLUSTER") {
-  server.listen(PORT, (err) => { 
-    !err
-      ? logger.info(`Server iniciado en modo:[${process.env.SERVERMODE}] con id de Proceso [${process.pid}] en puesto [${PORT}]`)
-      : logger.error(`Server no funciona por error:[${err}`);
-  });
-    }}
+    cluster.on("exit", (worker, code, signal) => {
+      logger.info(`worker con id de proceso ${worker.process.pid} eliminado`);
+    });
+  } else if (process.env.SERVERMODE == "CLUSTER") {
+    server.listen(PORT, (err) => {
+      !err
+        ? logger.info(
+            `Server iniciado en modo:[${process.env.SERVERMODE}] con id de Proceso [${process.pid}] en puerto [${PORT}]`
+          )
+        : logger.error(`Server no funciona por error:[${err}`);
+    });
+  }
+}
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const { Router } = express;
 const router = new Router();
+const {productsDao} = require("../dao/index");
+
 
 router.get("/", (_req, res) => {
   res.render("index");
 });
+
+router.get("/test", async (req, res)=>{
+  const data = await productsDao.listAll()
+  res.render("products", {products: data})
+});
+
+
 
 
 module.exports = router;
